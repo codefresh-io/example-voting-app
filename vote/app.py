363a05e723f53a9ed3e57454ed89f77d0151a080
Dustin Van Buskirk <dev@vanbuskirk.me>
2018-08-20 12:31:41 -0700

    smoke vs sun

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Star Trek")
-option_b = os.getenv('OPTION_B', "Star Wars")
+option_a = os.getenv('OPTION_A', "Smoke")
+option_b = os.getenv('OPTION_B', "Sun")
 
 hostname = socket.gethostname()
 
