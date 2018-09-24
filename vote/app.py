ee3b90df34b1b642a7e0ad3df8ffdf7e6ce74c1f
francisco-codefresh <39525266+francisco-codefresh@users.noreply.github.com>
2018-09-24 10:29:57 -0600

    Update app.py

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Cats")
-option_b = os.getenv('OPTION_B', "Dogs")
+option_a = os.getenv('OPTION_A', "OptionA")
+option_b = os.getenv('OPTION_B', "OptionB")
 
 hostname = socket.gethostname()
 
